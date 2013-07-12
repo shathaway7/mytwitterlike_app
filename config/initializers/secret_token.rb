0f09fb83352c7b649e4e3112fcdc50ffdd7a2f73
shathaway7 <shathaway7@verizon.net>
2013-07-12 07:06:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MytwitterlikeApp::Application.config.secret_key_base = '0de6fe1179f8b56f3d5108b6b05b1dfcea3f6a9f95497d028d00512c359a4c07d6b7825f95effdc3cf1940b8c6530a1c194fa8d25478f4f8162c7de1b2a7b751'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MytwitterlikeApp::Application.config.session_store :cookie_store, key: '_mytwitterlike_app_session'
